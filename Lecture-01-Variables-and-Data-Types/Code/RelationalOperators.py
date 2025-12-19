a8bf5701dbf5a5feccce78dcc85e29d660dfea7c
Reduan Ahmad <reduanahmad363737@gmail.com>
2025-12-20 01:39:15 +0600

    Add RelationalOperators.py for demonstrating relational operators in Python

---
+++ Lecture-01-Variables-and-Data-Types/Code/RelationalOperators.py
@@ -0,0 +1,12 @@
+
+
+#relational operators
+a = 10
+b = 20
+
+print(a == b)   #equal to | the answer is false
+print(a > b)    #greater than | the answer is false
+print(a < b)    #less than | the answer is true
+print(a != b)   #not equal to | the answer is true
+print(a >= b)   #greater than or equal to | the answer is false
+print(a <= b)   #less than or equal to | the answer is true
