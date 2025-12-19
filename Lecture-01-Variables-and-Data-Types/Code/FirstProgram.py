e395d6eefa38513a525bd97f9ac451fb1d154cff
Reduan Ahmad <reduanahmad363737@gmail.com>
2025-12-20 00:47:59 +0600

    Add initial Python scripts for variable demonstration and basic arithmetic

---
+++ Lecture-01-Variables-and-Data-Types/Code/FirstProgram.py
@@ -0,0 +1,15 @@
+
+print("Reduan Ahmad" , "Reduan is my name .")
+print(23+77)
+
+
+name = "Reduan Ahmad"
+ahmad = "Reduan is my name ."
+age = 23
+print("My name is", name , "and my age is", age)
+
+price = 9.99
+quntity = 4
+print("The price is", price , "dollars.")
+
+print("The total cost is", price * quntity , "dollars.")
---
+++ Lecture-01-Variables-and-Data-Types/Code/Variable.py
@@ -0,0 +1,9 @@
+
+name = "Reduan Ahmad" 
+age = 23
+price = 9.99
+
+
+print("My name is", name)
+print("My age is", age)
+print("The price is", price , "dollars.")
