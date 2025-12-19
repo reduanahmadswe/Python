94feddf70a09c9f5a9447902e020fcc59aef55a5
Reduan Ahmad <reduanahmad363737@gmail.com>
2025-12-20 01:45:43 +0600

    Add AssaignmentOperator.py and LogicalOperator.py for demonstrating assignment and logical operators in Python

---
+++ Lecture-01-Variables-and-Data-Types/Code/AssaignmentOperator.py
@@ -0,0 +1,7 @@
+num = 10
+num = num + 5  # num now becomes 15
+print("num : ",num)
+
+
+num **= 5
+print("num : ",num)
---
+++ Lecture-01-Variables-and-Data-Types/Code/LogicalOperator.py
@@ -0,0 +1,8 @@
+
+
+a = 49
+b = 20
+
+print(not (a > b))   #not greater than | the answer is false
+print((a > b) and (a == b))    #and operator | the answer is false
+print((a > b) or (a == b))    #or operator | the answer is true
